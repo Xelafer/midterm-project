4cb0838633141c72669f11e84dcc42f2b5ea3a9d
Alex Fernan Mercado <mercadoalex1416@gmail.com>
2021-07-01 22:47:10 +0800

    2nd commit

--- app.js
+++ app.js
@@ -1,9 +1,78 @@
 const menu  = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
+const navLogo = document.querySelector('#navbar__logo')
 
+//Display Mobile Menu
 const mobileMenu = () => {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
 }
 
 menu.addEventListener('click', mobileMenu)
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const exploreMenu = document.querySelector('#explore-page');
+    let scrollPos = window.scrollY;
+    // console.log(scrollPos);
+  
+    // adds 'highlight' class to my menu items
+    if (window.innerWidth > 960 && scrollPos < 600) {
+      homeMenu.classList.add('highlight');
+      aboutMenu.classList.remove('highlight');
+      return;
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+      aboutMenu.classList.add('highlight');
+      homeMenu.classList.remove('highlight');
+      exploreMenu.classList.remove('highlight');
+      return;
+    } else if (window.innerWidth > 960 && scrollPos < 2345) {
+      exploreMenu.classList.add('highlight');
+      aboutMenu.classList.remove('highlight');
+      return;
+    }
+  
+    if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+      elem.classList.remove('highlight');
+    }
+  };
+  
+  window.addEventListener('scroll', highlightMenu);
+  window.addEventListener('click', highlightMenu);
+  
+  //  Close mobile Menu when clicking on a menu item
+  const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth <= 768 && menuBars) {
+      menu.classList.toggle('is-active');
+      menuLinks.classList.remove('active');
+    }
+  };
+  
+  menuLinks.addEventListener('click', hideMobileMenu);
+  navLogo.addEventListener('click', hideMobileMenu);
+
+
+  //video hover effect
+const clip = document.querySelectorAll('.clip');
+  for (let i = 0; i <clip.length; i++) {
+      clip[i].addEventListener('mouseenter', function(e){
+        clip[i].play()
+      })
+      clip[i].addEventListener('mouseout', function(e){
+        clip[i].pause()
+      })
+  }
+
+//More Info
+
+const moreInfoBtn = document.querySelector('.tour__btn');
+const text = document.querySelector('.moreInfo')
+
+moreInfoBtn.addEventListener('click', (e)=>  {
+    text.classList.toggle('show-more');
+
+})
